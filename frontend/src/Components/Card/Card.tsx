269abe93f8e822643657cae9c443ed9043e2ecc8
OfficialHZ <trhzuser@gmail.com>
2025-11-13 16:11:33 -0600

    Initial commit

---
+++ frontend/src/Components/Card/Card.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import "./Card.css";
+
+interface Props {
+  companyName?: string;
+  ticker: string;
+  price: number;
+}
+
+const Card = ({ companyName, ticker, price }: Props) => {
+  return (
+    <div className="card">
+      <img
+        src="https://images.unsplash.com/photo-1506744038136-46273834b3fb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8c3RvY2t8ZW58MHx8MHx8fDA%3D&w=1000&q=80"
+        alt="Image"
+      />
+
+      <div className="details">
+        <h2>{companyName} ({ticker})</h2>
+        <p>${price}</p>
+      </div>
+      <p className="info">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia,
+        impedit.
+      </p>
+    </div>
+  );
+};
+
+export default Card;
